fdc661db4c3b2e352abc3fed39f3580c5417814e
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-11-08 11:46:11 +0800

    fixed the code and refactor

--- script/script.js
+++ script/script.js
@@ -1,36 +1,37 @@
-import Task from "./task.js";
-import { setID as id, removeValue as remove, renderTask as render } from "./taskHelpers.js";
-import modal, {
+import Task from "./task.class.js";
+import { setID as id, clearInputs as remove, renderTask as render } from "./task.helpers.js";
+import modalManager, {
    openModal as open,
    closeModal as close,
    setError as err,
-} from "./modals.js";
+} from "./modal.manager.js";
 
 // Modal
-modal.addBtn.addEventListener("click", open(modal.addModal, modal.overlay));
-modal.closeBtn.addEventListener("click", close(modal.addModal, modal.overlay));
-modal.errBtn.addEventListener("click", close(modal.errModal));
+modalManager.addModalOpenBtn.addEventListener("click", open(modalManager.addModal, modalManager.overlay__screen));
+modalManager.addModalBtnClose.addEventListener("click", close(modalManager.addModal, modalManager.overlay__screen))
+modalManager.editModalCloseBtn.addEventListener("click", close(modalManager.editModal, modalManager.overlay__screen));
+modalManager.errBtn.addEventListener("click", close(modalManager.errModal));
 render()
 
 // Add Task
-modal.saveBtn.addEventListener("click", (e) => {
-   let container = document.querySelector(".todo__container");
-   let task = document.getElementById("add__task");
-   let deadline = document.getElementById("add__task__due");
-   let cls = close(modal.addModal, modal.overlay);
+modalManager.saveBtn.addEventListener("click", (e) => {
+   let container = document.querySelector(".main__todo__container");
+   let task = document.getElementById("add__modal__task");
+   let deadline = document.getElementById("add__modal__task__due");
 
    if (!task.value.trim() || !deadline.value.trim()) {
       return err("Invalid Input!");
    }
 
-   let [due, ] = deadline.value.split("T")
+   let [due] = deadline.value.split("T")
 
    let storage = JSON.parse(localStorage.getItem("storage")) || [];
    storage.push(new Task(id(), task.value, due));
    localStorage.setItem("storage", JSON.stringify(storage));
 
-   remove(task, deadline);
    container.innerHTML = ''
    render()
-   cls();
+   remove(task, deadline);
+   close(modalManager.addModal, modalManager.overlay__screen)();
+   
 });
---
+++ script/task.helpers.js
@@ -0,0 +1,72 @@
+import modal, { openModal as open, closeModal as close } from "./modal.manager.js";
+
+export function setID() {
+   let current_id = JSON.parse(localStorage.getItem("id")) || 1;
+   let [old_value, new_value] = [current_id, ++current_id];
+   localStorage.setItem("id", JSON.stringify(new_value));
+   return old_value;
+}
+
+export function clearInputs(...inputs) {
+   for (let input of inputs) {
+      input.value = "";
+   }
+}
+
+export function renderTask() {
+   let storage = JSON.parse(localStorage.getItem("storage"));
+   if (!storage || storage == '') return;
+
+   storage.forEach((task) => {
+      let el = document.createElement("div");
+      el.setAttribute("class", `todo todo__${task.id}`);
+      el.innerHTML = 
+         `
+         <div class="todo__info" dataset-id=${task.id}>
+            <div class="todo__info__left">
+               <input type="radio" id="todo__radio__${task.id}" value="${task.id}" />
+               <span id="todo__task"> ${task.task} </span>
+            </div>
+            <div class="todo__info__right">
+               <div class="todo__info__btns">
+                  <img src="img/edit_calendar_17dp_434343_FILL0_wght400_GRAD0_opsz20.svg" dataset-id="${task.id}" class="edit__btn"/>
+                  <img src="img/delete_17dp_434343_FILL0_wght400_GRAD0_opsz20.svg" dataset-id="${task.id}" class="dlt__btn" />
+               </div>
+               <span id="todo__date"> ${task.deadline} </span>
+            </div>
+         </div> 
+         `;
+
+      document.querySelector(".main__todo__container").append(el);
+      attachListeners(task);
+   });
+}
+
+function attachListeners(task) {
+   let parent = document.querySelector(`.todo__${task.id}`);
+   parent.querySelector("input").addEventListener("click", (e) => {
+      parent.querySelector("span").classList.toggle("done");
+   });
+
+   let btns = parent.querySelector(".todo__info__btns");
+   for (let btn of btns.children) {
+      if (btn.classList.contains("edit__btn")) {
+         btn.addEventListener("click", (e) => {
+            open(modal.editModal, modal.overlay__screen)();
+         });
+         continue;
+      }
+
+      btn.addEventListener("click", (e) => {
+         let storage = JSON.parse(localStorage.getItem("storage"))
+         storage.forEach(currentTask => {
+            if (currentTask.id = task.id) {
+               storage.splice(currentTask.id - 1, 1)
+               return
+            }
+         })
+         localStorage.setItem("storage", JSON.stringify(storage))
+         parent.remove()
+      });
+   }
+}
