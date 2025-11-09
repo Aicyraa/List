748050695dc2d9434f9e12076fd1ba6356d2b33b
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-11-09 09:14:03 +0800

    finalized the script

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 import Task from "./task.class.js";
-import { setID as id, clearInputs as clear, renderTask as render, processDate, editData} from "./task.helpers.js";
+import {
+   setID as id,
+   clearInputs as clear,
+   renderTask,
+   processDate,
+   editData,
+} from "./task.helpers.js";
 import modalManager, {
    openModal as open,
    closeModal as close,
@@ -7,15 +13,23 @@ import modalManager, {
 } from "./modal.manager.js";
 
 // Modal
-modalManager.addModalOpenBtn.addEventListener("click", open(modalManager.addModal, modalManager.overlay__screen));
-modalManager.addModalBtnClose.addEventListener("click", close(modalManager.addModal, modalManager.overlay__screen))
-modalManager.editModalCloseBtn.addEventListener("click", close(modalManager.editModal, modalManager.overlay__screen));
+modalManager.addModalOpenBtn.addEventListener(
+   "click",
+   open(modalManager.addModal, modalManager.overlay__screen)
+);
+modalManager.addModalBtnClose.addEventListener(
+   "click",
+   close(modalManager.addModal, modalManager.overlay__screen)
+);
+modalManager.editModalCloseBtn.addEventListener(
+   "click",
+   close(modalManager.editModal, modalManager.overlay__screen)
+);
 modalManager.errBtn.addEventListener("click", close(modalManager.errModal));
-render()
+renderTask();
 
 // Add Task
 modalManager.saveBtn.addEventListener("click", (e) => {
-   let container = document.querySelector(".main__todo__container");
    let task = document.getElementById("add__modal__task");
    let deadline = document.getElementById("add__modal__task__due");
 
@@ -23,34 +37,32 @@ modalManager.saveBtn.addEventListener("click", (e) => {
       return err("Invalid Input!");
    }
 
-   let [date, time] = processDate(deadline)
+   let [date, time] = processDate(deadline);
 
    let storage = JSON.parse(localStorage.getItem("storage")) || [];
    storage.push(new Task(id(), task.value, date));
    localStorage.setItem("storage", JSON.stringify(storage));
 
-   container.innerHTML = ''
-   render()
+   renderTask();
    clear(task, deadline);
    close(modalManager.addModal, modalManager.overlay__screen)();
-   
 });
 
 modalManager.editModalSavebtn.addEventListener("click", () => {
    let editingId, parent, currentTask, currentDate;
-   editingId = modalManager.editModal.dataset.task
-   parent = document.querySelector(`.todo__${editingId}`)
-   currentTask = parent.querySelector("#todo__task").textContent
-   currentDate = parent.querySelector("#todo__date").textContent
+   editingId = modalManager.editModal.dataset.task;
+   parent = document.querySelector(`.todo__id-${editingId}`);
+   currentTask = parent.querySelector("#todo__task").textContent;
+   currentDate = parent.querySelector("#todo__date").textContent;
 
    let [newTask, newDate] = [
       modalManager.editModal.querySelector("textarea"),
       modalManager.editModal.querySelector("input"),
-   ]
+   ];
 
-   let [date] = processDate(newDate)
+   let [date] = processDate(newDate);
 
-   editData(parent, newTask.value || currentTask, date || currentDate)
-   clear(newTask, newDate)
-   close(modalManager.editModal, modalManager.overlay__screen)()
-})
+   editData(parent, newTask.value || currentTask, date || currentDate);
+   clear(newTask, newDate);
+   close(modalManager.editModal, modalManager.overlay__screen)();
+});
